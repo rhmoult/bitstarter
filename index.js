cd96404723cc02c6933ea6add5dc1b20ec0bc22b
Ubuntu <ubuntu@ip-172-31-58-197.ec2.internal>
2015-02-03 10:47:25 +0000

    Fixed missing require statement

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express();
 
+fs = require("fs");
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-html_string = fs.readFileSync('./index.html', 'utf-8')
+html_string = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send(html_string)
+  response.send(html_string.toString('utf-8'))
 })
 
 app.listen(app.get('port'), function() {
